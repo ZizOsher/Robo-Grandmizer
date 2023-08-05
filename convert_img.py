93d8d03ccd7a273a2c73ad677bb5951f76a10058
Osher <zizosher@gmail.com>
2023-08-05 11:53:26 +0300

    Changes to binary image and its conversion

--- convert_img.py
+++ convert_img.py
@@ -2,7 +2,8 @@ from PIL import Image
 import numpy as np
 import os
 
-image_path = 'bitmaps/floor-3.png'
+# image_path = 'bitmaps/floor-3.png'
+image_path = 'csfloor.png'
 
 # Open the image file
 with Image.open(image_path) as img:
