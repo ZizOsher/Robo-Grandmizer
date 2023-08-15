6abec8a030e9ba6c3889b72d34289340d064ed46
Osher <zizosher@gmail.com>
2023-08-15 12:37:08 +0300

    updated images

--- valid_cells.py
+++ valid_cells.py
@@ -1,6 +1,6 @@
 with open('binary_image.txt', 'r') as infile, open('output.txt', 'w') as outfile:
     for i, line in enumerate(infile):
         for j, val in enumerate(line.split()):
-            if val == '1':
+            if val == '0':
                 outfile.write('({}, {})\n'.format(i, j))
 
