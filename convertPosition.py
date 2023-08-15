98c4a16c7fd6a405d0271b347fce6a5cdbbd553d
Osher <zizosher@gmail.com>
2023-08-15 12:03:53 +0300

    distance convertions

--- convertPosition.py
+++ convertPosition.py
@@ -15,6 +15,15 @@ def pixel_to_world(px, py, image_width=287, image_height=369, world_width=43.780
     
     return x, y
 
+def world_distance_to_pixel_distance(distance, image_width=287, world_width=43.780):
+    """Convert a distance in the simulation world to a distance in pixels."""
+    return int(round(distance * image_width / world_width))
+
+def pixel_distance_to_world_distance(pixel_distance, image_width=287, world_width=43.780):
+    """Convert a distance in pixels to a distance in the simulation world."""
+    return round(pixel_distance * world_width / image_width, 6)
+
+
 # Test 1
 x, y = -8, -14
 px, py = world_to_pixel(x, y)
@@ -27,3 +36,10 @@ x, y = 0, 0
 px, py = pixel_to_world(x, y)
 x_new, y_new = world_to_pixel(px, py)
 print(f"Pixel: ({x}, {y}) -> World: ({px}, {py}) -> Pixel: ({x_new}, {y_new})")
+
+# Distance Test
+dist_meters = 10
+dist_pixels = world_distance_to_pixel_distance(dist_meters)
+dist_meters_new = pixel_distance_to_world_distance(dist_pixels)
+
+print(f"World distance: {dist_meters} meters -> Pixel distance: {dist_pixels} pixels -> World distance: {dist_meters_new} meters")
